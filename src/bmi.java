8398e5f322d77ee2fe3878d447ae051190728595
tadiwa thomas nyamayaro <tnyamay1@stu.vistula.edu.pl>
2023-10-27 17:49:17 +0200

    echo "# bmi" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/thomastadiwanyamayaro/bmi.git git push -u origin main

--- src/bmi.java
+++ src/bmi.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class bmi {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        double height  , mass, BMI;
+        double height  , mass, BMI;//
 
         System.out.println("please enter height in meters");
          height = sc.nextDouble();
