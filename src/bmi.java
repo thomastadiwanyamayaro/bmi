12c88c897e41302ba952d6811f9bf0c3f1588cda
tadiwa thomas nyamayaro <tnyamay1@stu.vistula.edu.pl>
2023-10-27 17:48:26 +0200

    echo "# bmi" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/thomastadiwanyamayaro/bmi.git git push -u origin main

---
+++ src/bmi.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class bmi {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        double height  , mass, BMI;
+
+        System.out.println("please enter height in meters");
+         height = sc.nextDouble();
+
+        System.out.println("please enter mass in kg");
+        mass = sc.nextDouble();
+
+         BMI = mass / (height * height);
+        System.out.print("The Body Mass Index (BMI) is " + BMI + " kg/m2");
+
+
+
+
+
+
+    }
+}
